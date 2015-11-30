276047487f97ea5a4274018e4e077f7a99583ac9
Mariano Goldman <mariano.goldman@mulesoft.com>
2015-11-30 09:50:09 -0300

    CC-160 | Fix complex type detection

--- src/main/java/org/mule/tools/devkit/sonar/utils/ClassParserUtils.java
+++ src/main/java/org/mule/tools/devkit/sonar/utils/ClassParserUtils.java
@@ -1,58 +1,116 @@
 package org.mule.tools.devkit.sonar.utils;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.sonar.plugins.java.api.tree.AnnotationTree;
-import org.sonar.plugins.java.api.tree.IdentifierTree;
-import org.sonar.plugins.java.api.tree.Tree;
-import org.sonar.plugins.java.api.tree.VariableTree;
+import org.sonar.api.batch.ProjectClasspath;
+import org.sonar.plugins.java.api.tree.*;
 
 import javax.annotation.Nullable;
 import java.math.BigDecimal;
+import java.util.*;
 
 public class ClassParserUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(ClassParserUtils.class);
 
-    private static final ImmutableSet<String> allowedComplexTypes;
+    private static final Set<String> primitives = new HashSet<>();
+
+    public static final ThreadLocal<ProjectClasspath> PROJECT_CLASSPATH_THREAD_LOCAL = new ThreadLocal<>();
 
     static {
-        final ImmutableSet.Builder<String> builder = ImmutableSet.builder();
-        builder.add(Integer.class.getSimpleName());
-        builder.add(Double.class.getSimpleName());
-        builder.add(Boolean.class.getSimpleName());
-        builder.add(Float.class.getSimpleName());
-        builder.add(Long.class.getSimpleName());
-        builder.add(Character.class.getSimpleName());
-        builder.add(Byte.class.getSimpleName());
-        builder.add(Short.class.getSimpleName());
-        builder.add(BigDecimal.class.getSimpleName());
-        builder.add(String.class.getSimpleName());
-        builder.add(Enum.class.getSimpleName());
+        primitives.add("int");
+        primitives.add("double");
+        primitives.add("long");
+        primitives.add("float");
+        primitives.add("char");
+        primitives.add("byte");
+        primitives.add("short");
+        primitives.add("boolean");
+    }
+
+    final private static Set<String> defaultImportedClasses = new HashSet<>();
+
+    static {
+        defaultImportedClasses.add("Integer");
+        defaultImportedClasses.add("Double");
+        defaultImportedClasses.add("Long");
+        defaultImportedClasses.add("Float");
+        defaultImportedClasses.add("Character");
+        defaultImportedClasses.add("Byte");
+        defaultImportedClasses.add("Short");
+        defaultImportedClasses.add("Boolean");
+        defaultImportedClasses.add("Object");
+        defaultImportedClasses.add("String");
+    }
+
+    final private static Map<String, Class<?>> primitiveToBoxedType = new HashMap<>();
+
+    static {
+        primitiveToBoxedType.put("int", java.lang.Integer.class);
+        primitiveToBoxedType.put("double", java.lang.Double.class);
+        primitiveToBoxedType.put("long", java.lang.Long.class);
+        primitiveToBoxedType.put("float", java.lang.Float.class);
+        primitiveToBoxedType.put("char", java.lang.Character.class);
+        primitiveToBoxedType.put("byte", java.lang.Byte.class);
+        primitiveToBoxedType.put("short", java.lang.Short.class);
+        primitiveToBoxedType.put("boolean", java.lang.Boolean.class);
+    }
+
+    private static final ImmutableSet<Class<?>> allowedComplexTypes;
+
+    static {
+        final ImmutableSet.Builder<Class<?>> builder = ImmutableSet.builder();
+        builder.add(Integer.class);
+        builder.add(Double.class);
+        builder.add(Long.class);
+        builder.add(Float.class);
+        builder.add(Character.class);
+        builder.add(Byte.class);
+        builder.add(Short.class);
+        builder.add(Boolean.class);
+        builder.add(BigDecimal.class);
+        builder.add(String.class);
+        builder.add(Enum.class);
+        builder.add(Date.class);
         allowedComplexTypes = builder.build();
     }
 
     private ClassParserUtils() {
     }
 
-    public static final Predicate<VariableTree> COMPLEX_TYPE_PREDICATE = new Predicate<VariableTree>() {
+    public static final Predicate<VariableTree> getComplexTypePredicate(final Set<ImportTree> imports) {
+        return new Predicate<VariableTree>() {
 
-        @Override
-        public boolean apply(@Nullable VariableTree input) {
-            return input != null && input.type().is(Tree.Kind.IDENTIFIER) && !isSimpleType((IdentifierTree) input.type());
-        }
-    };
+            @Override
+            public boolean apply(@Nullable VariableTree input) {
+                return input != null && input.type().is(Tree.Kind.IDENTIFIER) && !isSimpleType(input.type(), imports);
+            }
+        };
+    }
 
-    public static boolean isSimpleType(@NonNull final IdentifierTree type) {
+    public static boolean isPrimitive(@NonNull final Tree type, @NonNull Set<ImportTree> imports) {
+        Optional<Class<?>> clazz = classForName(type, imports);
+        return clazz.isPresent() && primitiveToBoxedType.values().contains(clazz.get());
+    }
 
-        // TODO We should find a better way to implement this. It's a bit naive of an implementation. Could get idea's from Paulo's code
-//        boolean result = symbolType.isPrimitive() || symbolType.symbol().isEnum() || (symbolType.isClass() && allowedComplexTypes.contains(symbolType.fullyQualifiedName()));
-        boolean result = allowedComplexTypes.contains(type.name());
+    public static boolean isSimpleType(@NonNull final Tree type, @NonNull Set<ImportTree> imports) {
+        Optional<Class<?>> clazz = classForName(type, imports);
+        boolean result = isPrimitive(type, imports) || isEnum(type, imports) || (clazz.isPresent() && allowedComplexTypes.contains(clazz.get()));
         logger.debug("Type '{}' is a simple type -> '{}'", type.toString(), result);
         return result;
+    }
+
+    public static boolean isEnum(@NonNull final Tree type, @NonNull final Set<ImportTree> imports) {
+        final Optional<Class<?>> optional = classForName(type, imports);
+        return optional.isPresent() && optional.get().isEnum();
     }
 
     public static boolean is(@NonNull AnnotationTree annotation, @NonNull final Class<?> annotationClass) {
@@ -60,4 +118,120 @@ public class ClassParserUtils {
         return annotationSimpleName.equals(annotationClass.getSimpleName()) || annotationSimpleName.equals(annotationClass.getCanonicalName());
     }
 
+    public static Optional<Class<?>> classForName(@NonNull final String classNameDef, @NonNull final Set<ImportTree> imports) {
+
+        // Is a generic declaration ?. Remove generic type ..
+        final String className = classNameDef.split("<")[0];
+
+        // Is the class name fully qualified ?
+        Optional<Class<?>> result = Optional.absent();
+        final boolean isFullQualified = className.contains(".");
+        if (isFullQualified) {
+            result = findClass(className);
+        }
+
+        // Object is a primitive class type ?
+        if (!result.isPresent()) {
+            if (primitives.contains(className)) {
+                result = Optional.<Class<?>>fromNullable(primitiveToBoxedType.get(className));
+            } else if (defaultImportedClasses.contains(className)) {
+                result = findClass("java.lang." + className);
+            }
+        }
+
+        // Type to be resolved based on the imports ...
+        if (!result.isPresent()) {
+            final Predicate<ImportTree> predicate = new Predicate<ImportTree>() {
+
+                @Override
+                public boolean apply(@Nullable ImportTree input) {
+                    return !input.isStatic() && extractImport(input.qualifiedIdentifier()).endsWith("." + className);
+                }
+            };
+            if (Iterables.any(imports, predicate)) {
+                final ImportTree importTree = Iterables.find(imports, predicate);
+                final String qualifiedName = extractImport(importTree.qualifiedIdentifier());
+                result = findClass(qualifiedName);
+            }
+        }
+
+        if (!result.isPresent()) {
+            result = Optional.<Class<?>>fromNullable(
+                    Iterables.getFirst(Iterables.transform(Iterables.filter(Iterables.transform(Iterables.transform(Iterables.filter(imports, new Predicate<ImportTree>() {
+
+                        @Override
+                        public boolean apply(@Nullable ImportTree input) {
+                            return !input.isStatic() && extractImport(input.qualifiedIdentifier()).endsWith(".*");
+                        }
+                    }), new Function<ImportTree, String>() {
+
+                        @Override
+                        public String apply(@Nullable ImportTree input) {
+                            final String statementStr = input.qualifiedIdentifier().toString();
+                            return statementStr.substring(0, statementStr.length() - 1) + className;
+                        }
+                    }), new Function<String, Optional<Class<?>>>() {
+
+                        @Override
+                        public Optional<Class<?>> apply(@Nullable String input) {
+                            return findClass(input);
+                        }
+                    }), new Predicate<Optional<Class<?>>>() {
+
+                        @Override
+                        public boolean apply(@Nullable Optional<Class<?>> input) {
+                            return input.isPresent();
+                        }
+
+                    }), new Function<Optional<Class<?>>, Class<?>>() {
+
+                        @Override
+                        public Class<?> apply(@Nullable Optional<Class<?>> input) {
+                            return input.get();
+                        }
+                    }), null));
+        }
+
+        if (!result.isPresent()) {
+            logger.warn("Class name can not be loaded '{}'", className);
+        }
+        return result;
+    }
+
+    @NonNull
+    private static Optional<Class<?>> findClass(@NonNull String className) {
+        Optional<Class<?>> result = Optional.absent();
+        try {
+            ProjectClasspath projectClasspath = PROJECT_CLASSPATH_THREAD_LOCAL.get();
+            result = Optional.<Class<?>>fromNullable(Class.forName(className, true, projectClasspath != null ? projectClasspath.getClassloader() : ClassParserUtils.class.getClassLoader()));
+        } catch (ClassNotFoundException e) {
+            // Ignore ...
+        }
+        return result;
+    }
+
+    public static Optional<Class<?>> classForName(@NonNull final Tree type, @NonNull final Set<ImportTree> imports) {
+        String classNameDef = extractType(type);
+        return classForName(classNameDef, imports);
+    }
+
+    private static String extractType(@NonNull Tree type) {
+        String classNameDef = type.toString();
+        if (type.is(Tree.Kind.VARIABLE)) {
+            classNameDef = ((VariableTree) type).type().toString();
+        }
+
+        return classNameDef;
+    }
+
+    private static String extractImport(Tree tree) {
+        List<String> list = Lists.newArrayList();
+        while (tree.is(Tree.Kind.MEMBER_SELECT)) {
+            final MemberSelectExpressionTree memberSelectExpressionTree = (MemberSelectExpressionTree) tree;
+            list.add(memberSelectExpressionTree.identifier().toString());
+            tree = memberSelectExpressionTree.expression();
+        }
+        list.add(tree.toString());
+        return Joiner.on(".").join(Iterables.reverse(list));
+    }
 }
