0666b0d4e7b50d570aed4d86efb394667487ac4b
Mariano Goldman <mariano.goldman@mulesoft.com>
2015-11-30 09:57:56 -0300

    CC-160 | Fix tests

--- src/test/files/licensechecks/LicenseByCategoryPremiumNoNameAttributeCheck.java
+++ src/test/files/licensechecks/LicenseByCategoryPremiumNoNameAttributeCheck.java
@@ -1,7 +1,3 @@
-/**
- * This file is the sample code against we run our unit test.
- * It is placed src/test/files in order to not be part of the maven compilation.
- **/
 import org.mule.api.annotations.Connector;
 import org.mule.api.annotations.Processor;
 import org.mule.api.annotations.licensing.RequiresEnterpriseLicense;
@@ -10,7 +6,7 @@ import org.mule.api.annotations.licensing.RequiresEntitlement;
 @Connector
 @RequiresEnterpriseLicense
 @RequiresEntitlement
-class LicenseByCategoryPremiumNoNameAttributeCheck {
+public class LicenseByCategoryPremiumNoNameAttributeCheck {
 
     @Processor
     public void aMethod() {
