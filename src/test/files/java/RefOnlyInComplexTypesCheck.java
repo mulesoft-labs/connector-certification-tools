b058c8426b3e4ac5a65fc630912aabce2814d0d3
Mariano Goldman <mariano.goldman@mulesoft.com>
2015-12-04 12:01:01 -0300

    CC-160 | Added Lists and Maps and simple types

--- src/test/files/java/RefOnlyInComplexTypesCheck.java
+++ src/test/files/java/RefOnlyInComplexTypesCheck.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.Map;
 import org.mule.api.annotations.Connector;
 import org.mule.api.annotations.Processor;
 
@@ -5,7 +7,25 @@ import org.mule.api.annotations.Processor;
 public class RefOnlyInComplexTypesCheck {
 
     @Processor
-    public void failingMethod(SomeComplexType s1) { // Noncompliant {{Processor 'failingMethod' contains variable 's1' of type 'SomeComplexType' (complex type) not annotated with @RefOnly.}}
+    public void failingMethod(SomeComplexType s1) { // Noncompliant {{Processor 'failingMethod' contains variable 's1' of type 'SomeComplexType' (complex type) not annotated with
+                                                    // @RefOnly.}}
     }
 
+    @Processor
+    public void aMethod(List<String> s1) {
+    }
+
+    @Processor
+    public void failingMethod(List<Object> s1) { // Noncompliant {{Processor 'failingMethod' contains variable 's1' of type 'List<Object>' (complex type) not annotated with
+                                                 // @RefOnly.}}
+    }
+
+    @Processor
+    public void aMethod(Map<String, String> m1) {
+    }
+
+    @Processor
+    public void failingMethod(Map<Long, SomeComplexType> m1) { // Noncompliant {{Processor 'failingMethod' contains variable 'm1' of type 'Map<Long, SomeComplexType>' (complex
+                                                               // type) not annotated with @RefOnly.}}
+    }
 }
