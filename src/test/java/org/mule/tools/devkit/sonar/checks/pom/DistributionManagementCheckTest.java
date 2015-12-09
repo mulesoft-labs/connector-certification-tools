1ca12b0bce5f99df5f353e33790510c3f30d7605
Cora Reyes Calens <urbanusjam@gmail.com>
2015-12-09 19:23:48 -0300

    CC-170 | Rule for Distribution Management in pom.xml

---
+++ src/test/java/org/mule/tools/devkit/sonar/checks/pom/DistributionManagementCheckTest.java
@@ -0,0 +1,63 @@
+package org.mule.tools.devkit.sonar.checks.pom;
+
+import com.google.common.collect.Iterables;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DistributionManagementCheckTest extends BasicPomTestBase {
+
+    @Test
+    public void checkNoDistributionManagement() throws IOException, XmlPullParserException {
+        final MavenProject mavenProject = createMavenProjectFromPom("DistributionManagementCheckTest-NoDistribution-pom.xml");
+        final DistributionManagementCheck check = new DistributionManagementCheck();
+        final Iterable<PomIssue> pomIssues = check.analyze(mavenProject);
+
+        assertThat(Iterables.size(pomIssues), is(1));
+        PomIssue pomIssue = Iterables.getOnlyElement(pomIssues);
+        assertThat(pomIssue.ruleKey(), is("distribution-management-by-category"));
+        assertThat(pomIssue.message(), is("Distribution Management must be properly configured in pom.xml under a <distributionManagement> tag, according to connector category."));
+    }
+
+    @Test
+    public void checkNoDeployRepo() throws IOException, XmlPullParserException {
+        final MavenProject mavenProject = createMavenProjectFromPom("DistributionManagementCheckTest-NoDeployRepo-pom.xml");
+        final DistributionManagementCheck check = new DistributionManagementCheck();
+        final Iterable<PomIssue> pomIssues = check.analyze(mavenProject);
+
+        assertThat(Iterables.size(pomIssues), is(1));
+        PomIssue pomIssue = Iterables.getOnlyElement(pomIssues);
+        assertThat(pomIssue.ruleKey(), is("distribution-management-by-category"));
+        assertThat(pomIssue.message(), is("Distribution Management is missing required <repository> configuration."));
+    }
+
+    @Test
+    public void checkNoSnapshotRepo() throws IOException, XmlPullParserException {
+        final MavenProject mavenProject = createMavenProjectFromPom("DistributionManagementCheckTest-NoSnapshotRepo-pom.xml");
+        final DistributionManagementCheck check = new DistributionManagementCheck();
+        final Iterable<PomIssue> pomIssues = check.analyze(mavenProject);
+
+        assertThat(Iterables.size(pomIssues), is(1));
+        PomIssue pomIssue = Iterables.getOnlyElement(pomIssues);
+        assertThat(pomIssue.ruleKey(), is("distribution-management-by-category"));
+        assertThat(pomIssue.message(), is("Distribution Management is missing required <snapshotRepository> configuration."));
+    }
+
+    @Test
+    public void checkWrongDistributionConfig() throws IOException, XmlPullParserException {
+        final MavenProject mavenProject = createMavenProjectFromPom("DistributionManagementCheckTest-WrongDistributionConfig-pom.xml");
+        final DistributionManagementCheck check = new DistributionManagementCheck();
+        final Iterable<PomIssue> pomIssues = check.analyze(mavenProject);
+
+        assertThat(Iterables.size(pomIssues), is(1));
+        PomIssue pomIssue = Iterables.getOnlyElement(pomIssues);
+        assertThat(pomIssue.ruleKey(), is("distribution-management-by-category"));
+        assertThat(pomIssue.message(), is("Distribution Management must be properly configured in pom.xml for Community category."));
+
+    }
+}
