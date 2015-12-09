2ac06a494936f130dc80ee80f53b8eb24103b73e
Cora Reyes Calens <urbanusjam@gmail.com>
2015-12-09 11:29:56 -0300

    CC-183 | Javadocs

--- src/test/java/org/mule/modules/certification/automation/functional/MethodATestCases.java
+++ src/test/java/org/mule/modules/certification/automation/functional/MethodATestCases.java
@@ -1,4 +1,7 @@
 package org.mule.modules.certification.automation.functional;
 
+/**
+ * Dummy data for compliant test {@code FunctionalTestPerProcessorCheckTest}, which loads its data from file {@code FunctionalTestPerProcessorCheckMisplacedFile} in src/test/java.
+ */
 public class MethodATestCases {
 }
--- src/test/java/org/mule/modules/certification/automation/system/MethodCTestCases.java
+++ src/test/java/org/mule/modules/certification/automation/system/MethodCTestCases.java
@@ -1,4 +1,8 @@
 package org.mule.modules.certification.automation.system;
 
+/**
+ * Dummy data for non-compliant test 'runWithMisplacedTestFile()' of {@code FunctionalTestPerProcessorCheckTest}, which loads its data from file
+ * {@code FunctionalTestPerProcessorCheckMisplacedFile} in src/test/java.
+ */
 public class MethodCTestCases {
 }
