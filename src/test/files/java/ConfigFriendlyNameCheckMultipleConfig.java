93e9684c72940e7bbeecd246122801c2fa4dff88
Cora Reyes Calens <urbanusjam@gmail.com>
2015-12-14 14:20:24 -0300

    CC-195 | CC-201 | Fixed formatting issue

--- src/test/files/java/ConfigFriendlyNameCheckMultipleConfig.java
+++ src/test/files/java/ConfigFriendlyNameCheckMultipleConfig.java
@@ -14,13 +14,11 @@ public class MyConnectorOAuth1Config {
 }
 
 @OAuth2(friendlyName = "OAuth v2.0 Configuration")
-public class MyConnectorOAuth2Config { // Noncompliant {{For multiple connector configurations, if one or more are annotated with @OAuth2, the default friendly name must be 'OAuth
-                                       // 2.0' (instead of 'OAuth v2.0 Configuration').}}
+public class MyConnectorOAuth2Config { // Noncompliant {{For multiple connector configurations, if one or more are annotated with @OAuth2, the default friendly name must be 'OAuth 2.0' (instead of 'OAuth v2.0 Configuration').}}
 
 }
 
 @ConnectionManagement(friendlyName = "MYSSO 3.0")
-public class MyConnectorSAMLConfig { // Noncompliant {{Single connector configurations annotated with @ConnectionManagement or @Configuration must use 'Configuration' as friendly
-                                     // name (instead of 'MYSSO 3.0'). If there are multiple configurations, none of which is of type @OAuth or @OAuth2, use a brief description to
-                                     // specify the configuration name (e.g, 'SAML 2.0').}}
+public class MyConnectorSAMLConfig { // Noncompliant {{Single connector configurations annotated with @ConnectionManagement or @Configuration must use 'Configuration' as friendly name (instead of 'MYSSO 3.0'). If there are multiple configurations, none of which is of type @OAuth or @OAuth2, use a brief description to specify the configuration name (e.g, 'SAML 2.0').}}
+
 }
