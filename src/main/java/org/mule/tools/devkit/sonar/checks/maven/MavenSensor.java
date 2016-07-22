6c208aa29e9e56034b69eb71ed7b7505a0c58490
Gastón Ortiz <gaston.ortiz@mulesoft.com>
2016-07-22 00:45:01 -0300

    Fixed maven sensor to define pom when available.

--- src/main/java/org/mule/tools/devkit/sonar/checks/maven/MavenSensor.java
+++ src/main/java/org/mule/tools/devkit/sonar/checks/maven/MavenSensor.java
@@ -48,11 +48,12 @@ public class MavenSensor implements Sensor {
         FileSystem fileSystem = sensorContext.fileSystem();
         final MavenProject mavenProject = PomUtils.createMavenProjectFromPomFile(fileSystem.baseDir());
         if (PomUtils.isDevKitConnector(mavenProject)) {
-            final InputFile pomFile = Iterables.getOnlyElement(fileSystem.inputFiles(fileSystem.predicates().matchesPathPattern("pom.xml")));
-            for (MavenCheck mavenCheck : buildMavenChecks()) {
-                final Iterable<ConnectorIssue> analyse = mavenCheck.analyze(mavenProject);
-                for (ConnectorIssue issue : analyse) {
-                    logAndRaiseIssue(pomFile, issue);
+            for (InputFile pomFile : fileSystem.inputFiles(fileSystem.predicates().matchesPathPattern("pom.xml"))) {
+                for (MavenCheck mavenCheck : buildMavenChecks()) {
+                    final Iterable<ConnectorIssue> analyse = mavenCheck.analyze(mavenProject);
+                    for (ConnectorIssue issue : analyse) {
+                        logAndRaiseIssue(pomFile, issue);
+                    }
                 }
             }
         }
